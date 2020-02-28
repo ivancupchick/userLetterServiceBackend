e20a4d4cc363b88c2e957fed38b8fb06ba6b2a43
Ddobysh27 <dobysh.dmitry27@gmail.com>
2020-02-28 22:13:42 +0300

    test

--- api/database.php
+++ api/database.php
@@ -7,7 +7,7 @@
 	define('DB_USER', 'root');
 	define('DB_PASS', 'root1');
 	define('DB_NAME', 'mydb');
-
+//test
 	function connect() {
 	  $connect = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
