393285d8360f7abf67c753d002314d69d4566639
Ddobysh27 <dobysh.dmitry27@gmail.com>
2020-03-05 23:57:25 +0300

    changed name records

--- api/createLetter.php
+++ api/createLetter.php
@@ -4,115 +4,183 @@ require 'database.php';
 // Get the posted data.
 $postdata = file_get_contents("php://input");
 
-if(isset($postdata) && !empty($postdata))
-{
-  // Extract the data.
-  $request = json_decode($postdata);
+if (isset($postdata) && !empty($postdata)) {
+    // Extract the data.
+    $request = json_decode($postdata);
 
 
-  // Validate.
-  if(
-    trim($request->hash) === '' ||
-    trim($request->receiverAddress->komu->name) === '' ||
-    trim($request->receiverAddress->komu->surname) === '' ||
-    trim($request->receiverAddress->komu->otchestvo) === '' // ||
-    // trim() === ''
-  ) {
-    return http_response_code(400);
-  }
+    // Validate.
+    if (
+        trim($request->hash) === '' ||
+        trim($request->receiverAddress->komu->name) === '' ||
+        trim($request->receiverAddress->komu->surname) === '' ||
+        trim($request->receiverAddress->komu->otchestvo) === '' // ||
+        // trim() === ''
+    ) {
+        return http_response_code(400);
+    }
 
-  // Sanitize.
-  $hash = mysqli_real_escape_string($con, trim($request->hash));
-  $status = mysqli_real_escape_string($con, trim($request->status));
+    // Sanitize.
+    $hash = mysqli_real_escape_string($con, trim($request->hash));
+    $status = mysqli_real_escape_string($con, trim($request->status));
 
-  $isMejdunarond = mysqli_real_escape_string($con, trim($request->isMejdunarond));
+    $isMejdunarond = mysqli_real_escape_string($con, trim($request->isMejdunarond));
 
-  $r_ko_n = mysqli_real_escape_string($con, trim($request->receiverAddress->komu->name));
-  $r_ko_s = mysqli_real_escape_string($con, trim($request->receiverAddress->komu->surname));
-  $r_ko_o = mysqli_real_escape_string($con, trim($request->receiverAddress->komu->otchestvo));
+    $r_ko_n = mysqli_real_escape_string($con, trim($request->receiverAddress->komu->name));
+    $r_ko_s = mysqli_real_escape_string($con, trim($request->receiverAddress->komu->surname));
+    $r_ko_o = mysqli_real_escape_string($con, trim($request->receiverAddress->komu->otchestvo));
 
-  $r_ku_st = mysqli_real_escape_string($con, trim($request->receiverAddress->kuda->streetType));
-  $r_ku_sn = mysqli_real_escape_string($con, trim($request->receiverAddress->kuda->streetName));
-  $r_ku_noh = mysqli_real_escape_string($con, trim($request->receiverAddress->kuda->numberOfHouse));
-  $r_ku_nok = mysqli_real_escape_string($con, trim($request->receiverAddress->kuda->numberOfKorpus));
-  $r_ku_nof = mysqli_real_escape_string($con, trim($request->receiverAddress->kuda->numberOfFlat));
+    $r_o_st = mysqli_real_escape_string($con, trim($request->receiverAddress->adress->streetType));
+    $r_o_sn = mysqli_real_escape_string($con, trim($request->receiverAddress->adress->streetName));
+    $r_o_noh = mysqli_real_escape_string($con, trim($request->receiverAddress->adress->numberOfHouse));
+    $r_o_nok = mysqli_real_escape_string($con, trim($request->receiverAddress->adress->numberOfKorpus));
+    $r_o_nof = mysqli_real_escape_string($con, trim($request->receiverAddress->adress->numberOfFlat));
 
-  $r_index = mysqli_real_escape_string($con, trim($request->receiverAddress->index));
+    $r_o_o = mysqli_real_escape_string($con, trim($request->receiverAddress->adress->oblast));
+    $r_o_r = mysqli_real_escape_string($con, trim($request->receiverAddress->adress->region));
+    $r_o_tn = mysqli_real_escape_string($con, trim($request->receiverAddress->adress->townName));
+    $r_o_tot = mysqli_real_escape_string($con, trim($request->receiverAddress->adress->typeOfTown));
+    $r_o_c = mysqli_real_escape_string($con, trim($request->receiverAddress->adress->country));
 
-  $r_npn_o = mysqli_real_escape_string($con, trim($request->receiverAddress->nasPunktName->oblast));
-  $r_npn_r = mysqli_real_escape_string($con, trim($request->receiverAddress->nasPunktName->region));
-  $r_npn_tn = mysqli_real_escape_string($con, trim($request->receiverAddress->nasPunktName->townName));
-  $r_npn_tot = mysqli_real_escape_string($con, trim($request->receiverAddress->nasPunktName->typeOfTown));
+    $r_o_index = mysqli_real_escape_string($con, trim($request->receiverAddress->adress->index));
 
-  $o_ok_n = mysqli_real_escape_string($con, trim($request->otpravitelAddress->otKogo->name));
-  $o_ok_s = mysqli_real_escape_string($con, trim($request->otpravitelAddress->otKogo->surname));
-  $o_ok_o = mysqli_real_escape_string($con, trim($request->otpravitelAddress->otKogo->otchestvo));
+    $o_ok_n = mysqli_real_escape_string($con, trim($request->otpravitelAddress->otKogo->name));
+    $o_ok_s = mysqli_real_escape_string($con, trim($request->otpravitelAddress->otKogo->surname));
+    $o_ok_o = mysqli_real_escape_string($con, trim($request->otpravitelAddress->otKogo->otchestvo));
 
-  $o_a_o = mysqli_real_escape_string($con, trim($request->otpravitelAddress->adress->oblast));
-  $o_a_r = mysqli_real_escape_string($con, trim($request->otpravitelAddress->adress->region));
-  $o_a_tn = mysqli_real_escape_string($con, trim($request->otpravitelAddress->adress->townName));
-  $o_a_tot = mysqli_real_escape_string($con, trim($request->otpravitelAddress->adress->typeOfTown));
+    $o_a_o = mysqli_real_escape_string($con, trim($request->otpravitelAddress->adress->oblast));
+    $o_a_r = mysqli_real_escape_string($con, trim($request->otpravitelAddress->adress->region));
+    $o_a_tn = mysqli_real_escape_string($con, trim($request->otpravitelAddress->adress->townName));
+    $o_a_tot = mysqli_real_escape_string($con, trim($request->otpravitelAddress->adress->typeOfTown));
+    $o_a_c = mysqli_real_escape_string($con, trim($request->otpravitelAddress->adress->country));
+    $r_a_index = mysqli_real_escape_string($con, trim($request->receiverAddress->adress->index));
 
-  $o_a_st = mysqli_real_escape_string($con, trim($request->otpravitelAddress->adress->streetType));
-  $o_a_sn = mysqli_real_escape_string($con, trim($request->otpravitelAddress->adress->streetName));
-  $o_a_noh = mysqli_real_escape_string($con, trim($request->otpravitelAddress->adress->numberOfHouse));
-  $o_a_nok = mysqli_real_escape_string($con, trim($request->otpravitelAddress->adress->numberOfKorpus));
-  $o_a_nof = mysqli_real_escape_string($con, trim($request->otpravitelAddress->adress->numberOfFlat));
 
-  $dateAndTimeOfStartWay = mysqli_real_escape_string($con, (int)$request->dateAndTimeOfStartWay);
+    $o_a_st = mysqli_real_escape_string($con, trim($request->otpravitelAddress->adress->streetType));
+    $o_a_sn = mysqli_real_escape_string($con, trim($request->otpravitelAddress->adress->streetName));
+    $o_a_noh = mysqli_real_escape_string($con, trim($request->otpravitelAddress->adress->numberOfHouse));
+    $o_a_nok = mysqli_real_escape_string($con, trim($request->otpravitelAddress->adress->numberOfKorpus));
+    $o_a_nof = mysqli_real_escape_string($con, trim($request->otpravitelAddress->adress->numberOfFlat));
 
-  // Create.
-  $sql = "INSERT INTO `letters`(`id`,`hash`,`status`, `isMejdunarond`, `r_ko_n`, `r_ko_s`, `r_ko_o`, `r_ku_st`, `r_ku_sn`, `r_ku_noh`, `r_ku_nok`, `r_ku_nof`, `r_index`, `r_npn_o`, `r_npn_r`, `r_npn_tn`, `r_npn_tot`, `o_ok_n`, `o_ok_s`, `o_ok_o`, `o_a_o`, `o_a_r`, `o_a_tn`, `o_a_tot`, `o_a_st`, `o_a_sn`, `o_a_noh`, `o_a_nok`, `o_a_nof`, `dateAndTimeOfStartWay`) VALUES (null,'{$hash}','{$status}','{$isMejdunarond}','{$r_ko_n}','{$r_ko_s}','{$r_ko_o}','{$r_ku_st}','{$r_ku_sn}','{$r_ku_noh}','{$r_ku_nok}','{$r_ku_nof}','{$r_index}','{$r_npn_o}','{$r_npn_r}','{$r_npn_tn}','{$r_npn_tot}','{$o_ok_n}','{$o_ok_s}','{$o_ok_o}','{$o_a_o}','{$o_a_r}','{$o_a_tn}','{$o_a_tot}','{$o_a_st}','{$o_a_sn}','{$o_a_noh}','{$o_a_nok}','{$o_a_nof}','{$dateAndTimeOfStartWay}')";
-  // create table policies( id int not null auto_increment, number varchar(20), amount float, primary key(id));
 
-/*
+    $tof = mysqli_real_escape_string($con, trim($request->typeOfLetter));
+    $lt = mysqli_real_escape_string($con, trim($request->letterType));
+    $sm = mysqli_real_escape_string($con, trim($request->specMarks));
+    $lwav = mysqli_real_escape_string($con, trim($request->letterWithAnnouncedValue));
+    $lwp = mysqli_real_escape_string($con, trim($request->letterWithPrice));
 
-create table letters( id int not null auto_increment,
-hash varchar(200),
-status varchar(200),
-isMejdunarond varchar(200),
-r_ko_n varchar(200),
-r_ko_s varchar(200),
-r_ko_o varchar(200),
-r_ku_st varchar(200),
-r_ku_sn varchar(200),
-r_ku_noh varchar(200),
-r_ku_nok varchar(200),
-r_ku_nof varchar(200),
-r_index varchar(200),
-r_npn_o varchar(200),
-r_npn_r varchar(200),
-r_npn_tn varchar(200),
-r_npn_tot varchar(200),
-o_ok_n varchar(200),
-o_ok_s varchar(200),
-o_ok_o varchar(200),
-o_a_o varchar(200),
-o_a_r varchar(200),
-o_a_tn varchar(200),
-o_a_tot varchar(200),
-o_a_st varchar(200),
-o_a_sn varchar(200),
-o_a_noh varchar(200),
-o_a_nok varchar(200),
-o_a_nof varchar(200),
-dateAndTimeOfStartWay float,
-primary key(id))
 
-*/
 
-  if(mysqli_query($con,$sql))
-  {
-    http_response_code(201);
+    $dateAndTimeOfStartWay = mysqli_real_escape_string($con, (int)$request->dateAndTimeOfStartWay);
 
-    $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-    $id = mysqli_insert_id($con);
+    // Create.
+    $sql = "INSERT INTO `letters`(`id`,`hash`,`status`, `isMejdunarond`, `r_ko_n`, `r_ko_s`, `r_ko_o`, `r_ku_st`, `r_ku_sn`, `r_ku_noh`, `r_ku_nok`, `r_ku_nof`, `r_index`, `r_npn_o`, `r_npn_r`, `r_npn_tn`, `r_npn_tot`, `o_ok_n`, `o_ok_s`, `o_ok_o`, `o_a_o`, `o_a_r`, `o_a_tn`, `o_a_tot`, `o_a_st`, `o_a_sn`, `o_a_noh`, `o_a_nok`, `o_a_nof`, `dateAndTimeOfStartWay`) VALUES (null,'{$hash}','{$status}','{$isMejdunarond}','{$r_ko_n}','{$r_ko_s}','{$r_ko_o}','{$r_ku_st}','{$r_ku_sn}','{$r_ku_noh}','{$r_ku_nok}','{$r_ku_nof}','{$r_index}','{$r_npn_o}','{$r_npn_r}','{$r_npn_tn}','{$r_npn_tot}','{$o_ok_n}','{$o_ok_s}','{$o_ok_o}','{$o_a_o}','{$o_a_r}','{$o_a_tn}','{$o_a_tot}','{$o_a_st}','{$o_a_sn}','{$o_a_noh}','{$o_a_nok}','{$o_a_nof}','{$dateAndTimeOfStartWay}')";
+    // create table policies( id int not null auto_increment, number varchar(20), amount float, primary key(id));
 
-    $letter = $actual_link . "/../downloadPDF.php?id=" . $id;
-    echo json_encode($letter);
-  }
-  else
-  {
-    http_response_code(422);
-  }
+    /*
+
+    create table letters( id int not null auto_increment,
+    hash varchar(200),
+    status varchar(200),
+    isMejdunarond varchar(200),
+    r_ko_n varchar(200),
+    r_ko_s varchar(200),
+    r_ko_o varchar(200),
+    r_ku_st varchar(200),
+    r_ku_sn varchar(200),
+    r_ku_noh varchar(200),
+    r_ku_nok varchar(200),
+    r_ku_nof varchar(200),
+    r_index varchar(200),
+    r_npn_o varchar(200),
+    r_npn_r varchar(200),
+    r_npn_tn varchar(200),
+    r_npn_tot varchar(200),
+    o_ok_n varchar(200),
+    o_ok_s varchar(200),
+    o_ok_o varchar(200),
+    o_a_o varchar(200),
+    o_a_r varchar(200),
+    o_a_tn varchar(200),
+    o_a_tot varchar(200),
+    o_a_st varchar(200),
+    o_a_sn varchar(200),
+    o_a_noh varchar(200),
+    o_a_nok varchar(200),
+    o_a_nof varchar(200),
+    dateAndTimeOfStartWay float,
+    primary key(id))
+
+    */
+
+    if (mysqli_query($con, $sql)) {
+        http_response_code(201);
+
+        $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        $id = mysqli_insert_id($con);
+
+        $letter = $actual_link . "/../downloadPDF.php?id=" . $id;
+        echo json_encode($letter);
+    } else {
+        http_response_code(422);
+    }
 }
+/*
+ export interface SimpleLeterInterface {
+  id: number;
+  hash: string;
+  status: LetterStatus;
+  isMejdunarond: string;
+
+  receiverAddress: {
+    komu:  {
+      name: string;
+      surname: string;
+      otchestvo: string;
+    };
+    adress: {
+      streetType?: StreetType; // enum
+      streetName?: string;
+      numberOfHouse: string;
+      numberOfKorpus?: string;
+      numberOfFlat?: string;
+      oblast: string; // область
+      region?: string; // район
+      townName: string; // город (название населеного пункта)
+      typeOfTown: TypeOfTown;
+      country: string;
+      index: string;
+    }
+
+  };
+
+  otpravitelAddress: {
+    otKogo:  {
+      name: string;
+      surname: string;
+      otchestvo: string;
+    },
+    adress: {
+      streetType?: StreetType; // enum
+      streetName?: string;
+      numberOfHouse: string;
+      numberOfKorpus?: string;
+      numberOfFlat?: string;
+      oblast: string; // область
+      region?: string; // район
+      townName: string; // город (название населеного пункта)
+      typeOfTown: TypeOfTown;
+      country: string;
+      index: string;
+    }
+  };
+
+   typeOfLetter: TypeOfLetter; // вид отправления
+   letterType?: LetterType; // if typeOfLetter === TypeOfLetter.pismo
+  specMarks: string; // string[].join(',') // if typeOfLetter === LetterType.zakaz
+   letterWithAnnouncedValue: 'true' | 'false';
+   letterWithPrice: 'true' | 'false';
+
+  dateAndTimeOfStartWay: number;
+}
+*/
